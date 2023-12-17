5ecbd40cd8884bae0c3ee21c941adca702072bc9
infamousgriefer <145841440+infamousgriefer@users.noreply.github.com>
2023-12-18 00:44:23 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const randomString = require("./randomstring")
+
+const string = randomString(8) // Generates a random string which is 8 characters long
+
+console.log(string) // Prints the random string generated (example: Vs7iJz7i, 4KL90O3u, KHn39zjn)
